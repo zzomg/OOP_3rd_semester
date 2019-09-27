2cb99be12e35015cd46c6c794d05e93e4326ff1b
Виктория <55043240+zzomg@users.noreply.github.com>
2019-09-27 13:59:44 +0700

    Update word_count.h

--- task0_intro/word_count.h
+++ task0_intro/word_count.h
@@ -11,33 +11,15 @@ typedef std::pair<std::string, long int> pair;
 
 class Parser
 {
-public:
-	Parser(std::string inputFilePath, std::string outputFilePath);
-	~Parser();
-
-	void parse_txt_();
 	void parse_line_(std::string& line);
-	std::vector<pair> sort_(std::map<std::string, long int>& map);
-	void push_csv_();
-
-	std::ifstream inputFile_;
-	std::ofstream outputFile_;
-
-private:
 	std::map<std::string, long int> words_;
 	long int totalWordsCount_;
+public:
+	Parser();
+	
+	void parse_txt_(const std::string &inputFilePath);
+	void push_csv_(const std::string &outputFilePath);
 };
 
-Parser::Parser(std::string inputFilePath, std::string outputFilePath)
-{
-	totalWordsCount_ = 0;
-	inputFile_.open(inputFilePath);
-	outputFile_.open(outputFilePath);
-};
 
-Parser::~Parser()
-{
-	inputFile_.close();
-	outputFile_.close();
-};
 
