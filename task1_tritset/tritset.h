012dae1800be992d1913d6834f77d5b6c6fabf67
Виктория <55043240+zzomg@users.noreply.github.com>
2019-10-11 17:36:12 +0700

    Update tritset.h

--- task1_tritset/tritset.h
+++ task1_tritset/tritset.h
@@ -12,10 +12,8 @@ Trit operator ~ (Trit value);
 
 class TritSet
 {
-public:
 	uint* storage_; //внутреннее хранилище
 	size_t size_; //количество ТРИТОВ в массиве
-	size_t capacity_; //количество UINT'ОВ в массиве
 
 public:
 
@@ -38,19 +36,19 @@ public:
 	friend class reference;
 
 	//constructors & a destructor
-	TritSet(size_t size = 0);
+	TritSet(size_t size = 0, Trit value = Trit::Unknown);
 	TritSet(const TritSet& other);
 	TritSet(TritSet&& other);
 	~TritSet();
 
 	//operators overloading
-	TritSet& operator=(TritSet& other);
+	TritSet& operator=(TritSet const& other);
 	reference operator[](size_t idx);
 	Trit operator[](size_t idx) const;
 	friend TritSet operator&(const TritSet& left, const TritSet& right);
 	friend TritSet operator|(const TritSet& left, const TritSet& right);
 	friend TritSet operator~(const TritSet& obj);
-	friend bool operator==(const TritSet& left, const TritSet& right);
+	friend bool operator==(const TritSet& left, const TritSet& right); 
 	friend bool operator!=(const TritSet& left, const TritSet& right);
 
 	//member functions
@@ -58,171 +56,18 @@ public:
 	size_t length();
 	void trim(size_t idx);
 	size_t cardinality(Trit value);
-	size_t get_capacity() { return capacity_; }
-	std::map< int, size_t > cardinality();
+	//size_t get_capacity() { return capacity_; }
+	size_t get_size() { return size_; }
+	std::map< Trit, size_t > cardinality();
 
 	//utilities
 	void reallocMemory(size_t idx);
+	static size_t tritsToUints(size_t trit_n) {
+		return (trit_n * 2 / 8 / sizeof(uint));
+	}
+	static size_t tritsInByte() {
+		return (sizeof(uint) * 8 / 2);
+	}
 	void setTrit(Trit value, size_t idx);
 	Trit getTrit(size_t idx) const;
-	static size_t tritsToBytes(size_t trit_n) {
-		return (trit_n * 2 / 8 / sizeof(uint));
-	}
-	static size_t tritsInByte() {
-		return (sizeof(uint) * 8 / 2);
-	}
 };
-
-
-
-
-
-/*
-//MY OLD VERSION
-#pragma once
-#include <map>
-
-using uint = unsigned int;
-
-enum Trit : uint { False = 2, Unknown = 0, True = 1 };
-//logic values: False = 10, Unknown = 00, True = 01
-
-class TritSet
-{
-	uint* storage_; //внутреннее хранилище
-	size_t size_; //количество ТРИТОВ в массиве
-	size_t capacity_; //количество UINT'ОВ в массиве
-
-public:
-
-	class reference
-	{
-		friend class TritSet;
-
-		uint* word_;
-		size_t bit_pos_;
-
-		reference();
-
-	public:
-		reference(TritSet& tritset, size_t idx);
-		~reference() {};
-
-		reference& operator=(int value);
-		reference& operator=(reference& other);
-	};
-
-	friend class reference;
-
-	//constructors & a destructor
-	TritSet(size_t size = 0);
-	TritSet(const TritSet& other);
-	TritSet(TritSet&& other);
-	~TritSet();
-
-	//operators overloading
-	TritSet& operator=(TritSet& other);
-	reference operator[](size_t idx);
-	uint operator[](size_t idx) const;
-
-	friend TritSet operator&(const TritSet& left, const TritSet& right);
-	friend TritSet operator|(const TritSet& left, const TritSet& right);
-	friend TritSet operator~(const TritSet& obj);
-
-	//member functions
-	void shrink();
-	size_t length();
-	void trim(size_t idx);
-	size_t cardinality(int value);
-	size_t get_capacity() { return capacity_; }
-	std::map< int, size_t > cardinality();
-
-	//utilities
-	void reallocMemory(size_t idx);
-	static size_t tritsToBytes(size_t trit_n) {
-		return (trit_n * 2 / 8 / sizeof(uint));
-	}
-	static size_t tritsInByte() {
-		return (sizeof(uint) * 8 / 2);
-	}
-};
-*/
-/*
-//SEMINAR VERSION 
-#pragma once
-#include "stdafx.h"
-#include "targetver.h"
-#include <map>
-
-using uint = unsigned int;
-
-enum Trit : uint { False = 2, Unknown = 0, True = 1 };
-//logic values: False = 10, Unknown = 00, True = 01
-
-Trit operator | (Trit left, Trit right);
-Trit operator & (Trit left, Trit right);
-Trit operator ~ (Trit value);
-
-class TritSet
-{
-	uint* storage_; //внутреннее хранилище
-	size_t size_; //количество ТРИТОВ в массиве
-	size_t capacity_; //количество UINT'ОВ в массиве
-
-public:
-
-	class reference
-	{
-		friend class TritSet;
-
-		uint* word_;
-		size_t bit_pos_;
-		size_t index;
-		TritSet& tritset;
-
-		reference();
-
-	public:
-		operator Trit() const;
-		reference(TritSet& tritset, size_t idx);
-		~reference() {};
-
-		reference& operator=(int value);
-		reference& operator=(reference& other);
-	};
-
-	friend class reference;
-
-	//constructors & a destructor
-	TritSet(size_t size = 0);
-	TritSet(const TritSet& other);
-	TritSet(TritSet&& other);
-	~TritSet();
-
-	//operators overloading
-	TritSet& operator=(TritSet& other);
-	reference operator[](size_t idx);
-	Trit operator[](size_t idx) const;
-
-	friend TritSet operator&(const TritSet& left, const TritSet& right);
-	friend TritSet operator|(const TritSet& left, const TritSet& right);
-	friend TritSet operator~(const TritSet& obj);
-
-	//member functions
-	void shrink();
-	size_t length();
-	void trim(size_t idx);
-	size_t cardinality(int value);
-	size_t get_capacity() { return capacity_; }
-	std::map< int, size_t > cardinality();
-
-	//utilities
-	static void reallocMemory(TritSet& tritset, size_t idx);
-	static size_t tritsToBytes(size_t trit_n) {
-		return (trit_n * 2 / 8 / sizeof(uint));
-	}
-	static size_t tritsInByte() {
-		return (sizeof(uint)* 8 / 2);
-	}
-};
-*/
