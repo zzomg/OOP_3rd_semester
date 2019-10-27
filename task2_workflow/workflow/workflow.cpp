129428f835cc30434a81ab57d9d368aba6288f41
Виктория <55043240+zzomg@users.noreply.github.com>
2019-10-27 21:02:47 +0700

    Workflow executor
    Execute instructions given in the file

---
+++ task2_workflow/workflow/workflow.cpp
@@ -0,0 +1,31 @@
+#include "workflow.h"
+#include "parser.h"
+#include "instr_factory.h"
+
+void Workflow::run(const std::string& workflowFilePath)
+{
+	std::ifstream workflowFile(workflowFilePath);
+	if (!workflowFile) {
+		throw std::exception("Workflow file opening failed\n");
+	}
+
+	Parser parser;
+	parser.parseWFFile(workflowFile);
+
+	InstrFactory& factory = InstrFactory::instance();
+
+	std::vector<int>::iterator it;
+	for (it = parser.instructions_order_.begin(); it != parser.instructions_order_.end(); ++it) {
+		if (it == parser.instructions_order_.begin() &&
+			parser.instructions_[*it].first != "readfile") {
+			throw std::exception("Workflow must start with \"readfile\"\n");
+		}
+		if (it == parser.instructions_order_.end() - 1 &&
+			parser.instructions_[*it].first != "writefile") {
+			throw std::exception("Workflow must end with \"writefile\"\n");
+		}
+		Instruction* instr = factory.Create(parser.instructions_[*it]);
+		instr->execute(input);
+		delete instr;
+	}
+}
