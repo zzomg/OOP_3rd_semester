644018e00f472db31821107c885088c88b6a3437
Виктория <55043240+zzomg@users.noreply.github.com>
2019-11-29 01:37:57 +0700

    Update csv_parser.cpp

--- task4_csv_parser/csv_parser.cpp
+++ task4_csv_parser/csv_parser.cpp
@@ -7,7 +7,7 @@
 #include <iterator>
 #include <type_traits>
 
-/*Методы для вывода tuple'ов*/
+//#include "csv_parser.h"
 
 template <size_t n, typename... Args>
 typename std::enable_if<(n >= sizeof...(Args))>::type
@@ -32,109 +32,95 @@ std::ostream& operator<<(std::ostream& os, const std::tuple<Args...>& tup)
 	return os << ")";
 }
 
-
-/*std::istream& operator>>(std::istream& str, CSVParser::CSVRow& data)
+template <typename T>
+T convert(const std::string& param)
 {
-	data.readNextRow(str);
-	return str;
-}*/
+	T t;
+	std::stringstream ss(param);
 
-template<typename First, typename... Args>
+	ss >> t;
+
+	return T(t);
+}
+
+template <>
+std::string convert(const std::string& param) //ничего конвертировать не нужно
+{
+	return param;
+}
+
+template <size_t n, typename... Args>
+typename std::enable_if<(n >= sizeof...(Args))>::type
+read_tuple(const std::vector<std::string>& columns, std::tuple<Args...>& tup)
+{
+}
+
+template <size_t n, typename... Args>
+typename std::enable_if<(n < sizeof...(Args))>::type
+	read_tuple(const std::vector<std::string>& columns, std::tuple<Args...>& tup)
+{
+	std::get<n>(tup) = convert<std::tuple_element<n, std::tuple<Args...>>::type>(columns[n]);
+	read_tuple<n + 1>(columns, tup);
+}
+
+template<typename... Args>
 class CSVParser
 {
 	std::ifstream& file_;
 	size_t start_;
-	std::tuple<Args...> t_;
-	std::vector<std::string> vec_;
+	size_t cnt_;
 
 public:
 
-	//CSVParser(std::ifstream& file, Args&&...args, size_t start = 0) : file_(file), start_(start), args_(std::make_tuple(std::forward<Args>(args)...)) {}
-	//CSVParser() {}
-	CSVParser(std::ifstream& file, size_t start = 0) : file_(file), start_(start) {}
-	~CSVParser() {}
-
-	/*template <typename A>
-	void process_one_type() 
+	CSVParser(std::ifstream& file, size_t start = 0) : file_(file), start_(start) 
 	{
-		std::cout << typeid(A).name() << ' ';
-		vec_.push_back(typeid(A).name());
-	}
-
-	template <typename First, typename... Args>
-	void func()
-	{
-		vec_.push_back(typeid(First).name());
-		int _[] = {0, (process_one_type<Args>(), 0)... };
-		(void)_;
-		std::cout << '\n';
-		for (auto& v : vec_) 
-		{
-			std::cout << v << std::endl;
+		std::string line;
+		for (size_t i = 0; i < start_; ++i) {
+			std::getline(file_, line);
 		}
 	}
-
-	template <typename First, typename... Args>
-	void get_types() 
-	{
-		func<Args>();
-		std::tuple<First> аb = std::make_tuple(3);
-		std::cout << ab << std::endl;
-	}*/
+	~CSVParser() {}
 
 	class CSVRow
 	{
-		std::vector<std::string> data_;
-		//std::tuple<Args...> data_;
+		std::tuple<Args...> data_;
 
 	public:
 
-		std::tuple<Args...> data() const
-		{
+		friend class CSVParser<Args...>;
+
+		operator std::tuple<Args...>() const {
 			return data_;
 		}
-
-		std::size_t size() const
-		{
-			return data_.size();
-		}
-
-		/*template <typename T>
-		bool read_tuple_element(std::istream& in, T& value) {
-			std::cout << value << std::endl;
-			in >> value;
-			return true;
-		}
-
-		template <typename Tuple, std::size_t... I>
-		void read_tuple_elements(std::istream& in, Tuple& value, std::index_sequence<I...>) {
-			std::initializer_list<bool>{ read_tuple_element(in, std::get<I>(value))...};
-		}
-
-		void readNextRow(std::istream& str)
-		{
-			read_tuple_elements(str, data_, std::make_index_sequence<sizeof...(Args)>{});
-		}*/
 
 		std::istream& readNextRow(std::istream& str)
 		{
 			std::string line;
 			std::getline(str, line);
 
+			std::vector<std::string> data;
+
 			std::stringstream lineStream(line);
 			std::string cell;
 
-			data_.clear();
-			while (std::getline(lineStream, cell, ','))
-			{
-				data_.push_back(cell);
+			while (std::getline(lineStream, cell, ',')) {	
+				data.push_back(cell);
 			}
-			// This checks for a trailing comma with no data after it.
-			if (!lineStream && cell.empty())
-			{
-				// If there was a trailing comma then add an empty element.
-				data_.push_back("");
+
+			if (data.empty()) {
+				return str;
 			}
+
+			if (!lineStream && cell.empty()) {
+				data.push_back("");
+			}
+
+			if (data.size() != sizeof... (Args)) {
+				throw std::exception("Number of template arguments must correspond to the number of words in each line in file\n");
+			}
+
+			read_tuple<0>(data, data_);
+
 			return str;
 		}
 	};
@@ -145,6 +131,7 @@ public:
 		CSVRow row_;
 
 	public:
+
 		typedef std::input_iterator_tag iterator_category;
 		typedef CSVRow value_type;
 		typedef std::size_t difference_type;
@@ -158,10 +145,7 @@ public:
 		CSVIterator() : str_(NULL) {}
 		~CSVIterator() {}
 
-
-		// Pre Increment
-		CSVIterator& operator++() 
-		{ 
+		CSVIterator& operator++() { 
 			if (str_) 
 			{ 
 				row_.readNextRow((*str_));
@@ -172,9 +156,8 @@ public:
 			}
 			return *this; 
 		}
-		// Post increment
-		CSVIterator operator++(int) 
-		{
+
+		CSVIterator operator++(int) {
 			CSVIterator tmp(*this); 
 			++(*this); 
 			return tmp; 
@@ -182,46 +165,43 @@ public:
 		CSVRow const& operator*() const { return row_; }
 		CSVRow const* operator->()  const { return &row_; }
 
-		bool operator==(CSVIterator const& rhs) 
-		{ 
+		bool operator==(CSVIterator const& rhs) { 
 			return ((this == &rhs) || ((this->str_ == NULL) && (rhs.str_ == NULL))); 
 		}
-		bool operator!=(CSVIterator const& rhs) 
-		{ 
+		bool operator!=(CSVIterator const& rhs) { 
 			return !((*this) == rhs); 
 		}
 	};
-	
-	//friend std::istream& operator>>(std::istream& str, CSVParser::CSVRow& data);
+
+	friend class CSVRow;
 
 	typedef CSVIterator iterator;
 
-	iterator begin() 
-	{
+	iterator begin() {
 		return CSVIterator(file_);
 	}
 
-	iterator end()
-	{
+	iterator end() {
 		return CSVIterator();
 	}
+
 };
 
 int main()
 {
-	std::ifstream file("D:/uma_challenge/images_labelling.csv");
-	CSVParser<int, int, std::string> parser(file);
-	//parser.get_types<int, int, std::string>();
+	std::ifstream file("test.txt");
+	CSVParser<int, int, std::string> parser(file, 1);
 
-	//for (CSVIterator loop(file); loop != CSVIterator(); ++loop)
-	//for (CSVParser<int, int, std::string>::CSVRow s : parser) {
-		//std::cout << "1st Element(" << (*loop)[0] << ")\n";
-		//std::cout << s.data() << std::endl;
-	//}
-
-	//std::cout << std::make_tuple(2, 3.14159F, 2345.678) << std::endl;
-	//std::cout << std::make_tuple("hello", 5) << std::endl;
-	//std::cout << std::make_tuple() << std::endl;
+	try {
+		for (std::tuple<int, int, std::string> s : parser)
+		{
+			std::cout << s << std::endl;
+		}
+	}
+	catch (const std::exception & ex)
+	{
+		std::cout << "Error : " << ex.what() << std::endl;
+	}
 
 	return 0;
 }
